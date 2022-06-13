789a3e2d1418e46092be57b9371d0940ebe461e0
Danji-ya <erangerang15@gmail.com>
2022-06-13 15:18:34 +0900

    [refactor]: 조건문 캡슐화

--- src/utils/numberMaker.ts
+++ src/utils/numberMaker.ts
@@ -2,18 +2,22 @@ import { Random } from '@woowacourse/mission-utils';
 
 import { INPUT_NUMBER } from '../constants';
 
+function isLessThanInputMaxLength(size: number) {
+  return size < INPUT_NUMBER.LENGTH;
+}
+
 function getRandomNumber(startInclusive: number, endInclusive: number) {
   return Random.pickNumberInRange(startInclusive, endInclusive);
 }
 
 function getComputerInputNumbers(): string {
-  const maker = new Set();
+  const numbers = new Set();
 
-  while (maker.size !== INPUT_NUMBER.LENGTH) {
-    maker.add(getRandomNumber(INPUT_NUMBER.MIN, INPUT_NUMBER.MAX));
+  while (isLessThanInputMaxLength(numbers.size)) {
+    numbers.add(getRandomNumber(INPUT_NUMBER.MIN, INPUT_NUMBER.MAX));
   }
 
-  return [...maker].join('');
+  return [...numbers].join('');
 }
 
 export { getRandomNumber, getComputerInputNumbers };
